b98f64065fe01152c8761a79cc431ec8d224d261
rett84 <lucasrettore31@hotmail.com>
2025-04-19 20:16:27 -0400

    aesd_circular_buffer_find_entry_offset_for_fpos implementation

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,28 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+
+    struct aesd_buffer_entry *entry;
+    entry = malloc(sizeof(struct aesd_buffer_entry));
+   
+    size_t entry_size_total = 0;
+    for (size_t i = 0; i < 10; i++)
+    {
+        entry_size_total = (*buffer).entry[i].size + entry_size_total;
+        if ((char_offset <= entry_size_total + (*buffer).entry[i+1].size) & (char_offset >= entry_size_total))
+        {
+            *entry_offset_byte_rtn = (char_offset - entry_size_total)-1;
+            *entry = (*buffer).entry[i+1];
+            return entry;
+        }
+        else if((char_offset <= entry_size_total))
+        {
+            *entry_offset_byte_rtn = char_offset;
+            *entry = (*buffer).entry[i];
+            return entry;
+        }
+        
+    }
     return NULL;
 }
 
@@ -44,9 +63,11 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
+ 
     /**
     * TODO: implement per description
     */
+
 }
 
 /**
@@ -55,4 +76,54 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
 void aesd_circular_buffer_init(struct aesd_circular_buffer *buffer)
 {
     memset(buffer,0,sizeof(struct aesd_circular_buffer));
+}
+
+static void write_circular_buffer_packet(struct aesd_circular_buffer *buffer,
+                                         const char *writestr)
+{
+    struct aesd_buffer_entry entry;
+    entry.buffptr = writestr;
+    entry.size=strlen(writestr);
+    aesd_circular_buffer_add_entry(buffer,&entry);
+}
+
+
+void main()
+{
+
+ 
+    struct aesd_circular_buffer buffer;
+    size_t offset_rtn=0;
+   // aesd_circular_buffer_init(&buffer);
+   // write_circular_buffer_packet(&buffer,"write1\n"); 
+    // for (size_t i = 0; i < 10; i++)
+    // {
+    //     char c[] ="write1\n";
+    //     //har msg[] = strcat("write",c);
+    //     (buffer).entry[i].buffptr= c;
+    //     (buffer).entry[i].size = strlen(c);
+    // }
+
+    (buffer).entry[0].buffptr= "escrita0\n";
+    (buffer).entry[0].size = strlen((buffer).entry[0].buffptr);
+
+    (buffer).entry[1].buffptr= "escrita12\n";
+    (buffer).entry[1].size = strlen((buffer).entry[1].buffptr);
+
+    (buffer).entry[2].buffptr= "escrita123456\n";
+    (buffer).entry[2].size = strlen((buffer).entry[2].buffptr);
+
+    (buffer).entry[3].buffptr= "escrita1234567890\n";
+    (buffer).entry[3].size = strlen((buffer).entry[3].buffptr);
+
+    (buffer).entry[4].buffptr= "escrita1234567890abcd\n";
+    (buffer).entry[4].size = strlen((buffer).entry[4].buffptr);
+    
+ 
+ // (buffer).entry[1].size = strlen("write1\n");
+     struct aesd_buffer_entry *rtnentry = aesd_circular_buffer_find_entry_offset_for_fpos(&buffer,
+                                                11,
+                                                &offset_rtn);
+
+   
 }
